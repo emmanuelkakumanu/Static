d3e1263e0a7d6d123404037f886f274c0b5e589f
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-15 19:34:08 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	static int a=10;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Ideone obj=new Ideone();
+		obj.fun();
+		
+	}
+	void fun()
+	{
+		//can access static variables in any non static methods.
+		System.out.print("Static variable a : "+a);
+	}
+}
